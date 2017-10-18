5623cb9ab624d9c6b5001aa47b5222528c7fa346
Luke Pirtle <luke.pirtle@techfino.com>
2017-10-18 16:11:55 -0700

    Documentation stuff
    Corrected header info

--- tf_lib_restlet_post_example.py
+++ tf_lib_restlet_post_example.py
@@ -1,3 +1,16 @@
+#
+# Copyright (c) 2014-2017 Techfino, LLC
+# 1900 Market Street, 8th Floor, Philadelphia, PA 19103, USA
+# All Rights Reserved.
+#
+# This software is the confidential and proprietary information of Techfino LLC.
+# ("Confidential Information") - You shall not disclose such Confidential Information
+# without prior written permission.
+#
+# Script Description:
+#  This script is the central library for webservices functions for Techfino SS 2.0
+#
+
 import oauth2 as oauth
 import json
 import requests
@@ -7,7 +20,7 @@ url = "https://rest.netsuite.com/.../restlet.nl?script=X&deploy=Y"
 token = oauth.Token(key="____________________________", secret="____________________________")
 consumer = oauth.Consumer(key="____________________________", secret="____________________________")
 http_method = "POST"
-realm = "123456" # NetSuite account id
+realm = "123456"  # NetSuite account id
 
 # the JSON data to be sent to the RESTlet
 payload = {
